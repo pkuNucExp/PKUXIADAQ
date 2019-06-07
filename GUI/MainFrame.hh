269b74e54723ac22eff771294c66097271f5938e
wuhongyi <wuhongyi@pku.edu.cn>
2019-06-07 21:54:38 +0800

    20190607

--- GUI/MainFrame.hh
+++ GUI/MainFrame.hh
@@ -4,15 +4,15 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 五 3月  9 13:01:17 2018 (+0800)
-// Last-Updated: 六 5月  4 20:39:00 2019 (+0800)
+// Last-Updated: 四 6月  6 16:54:11 2019 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 20
+//     Update #: 22
 // URL: http://wuhongyi.cn 
 
 #ifndef MAINFRAME_HH_
 #define MAINFRAME_HH_
 
-const char gVERSION[] = "Version: 2019.05.04";
+const char gVERSION[] = "Version: 2019.06.06";
 
 const char gAbout[] = "\
 Version: Beta ---> Developer version\n\
--- README/zh/README.md
+++ README/zh/README.md
@@ -4,9 +4,9 @@
 ;; Author: Hongyi Wu(吴鸿毅)
 ;; Email: wuhongyi@qq.com 
 ;; Created: 六 6月 18 13:37:42 2016 (+0800)
-;; Last-Updated: 四 6月  6 17:28:33 2019 (+0800)
+;; Last-Updated: 五 6月  7 21:49:40 2019 (+0800)
 ;;           By: Hongyi Wu(吴鸿毅)
-;;     Update #: 123
+;;     Update #: 124
 ;; URL: http://wuhongyi.cn -->
 
 # README
@@ -25,7 +25,7 @@
 
 ### 稳定版本
 
-**稳定版本 Version:2019.05.04**  
+**稳定版本 Version:2019.06.06**  
 
 下载最新版本，请点击:  [PKUXIADAQ stable](https://github.com/pkuNucExp/PKUXIADAQ)
 
