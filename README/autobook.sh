6d1e1c4accf5483d1253c997f33f5d7f0f66e496
wuhongyi <wuhongyi@qq.com>
2019-07-04 13:28:00 +0800

    20190704 uodate README form gitbook to Sphinx

--- README/autobook.sh
+++ README/autobook.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-rm -rf ../docs/*
+rm -rf ../docs/doctrees/
+rm -rf ../docs/en/*
+rm -rf ../docs/zh/*
 
-gitbook build
+cp index.html ../docs/
+touch ../docs/.nojekyll
 
-cp -r _book/*  ../docs/
+cd en/
+make clean
+make html
+mv build/doctrees/ ../../docs/
+cp -r build/html/* ../../docs/en/
+make clean
+cd ..
 
-rm -r _book
-
-cp en/README.md ..
-cp zh/README.md ../README_zh.md
-
-gitbook pdf . ../README.pdf
+cd zh/
+make clean
+make html
+cp -r build/html/* ../../docs/zh/
+make clean
+cd ../
